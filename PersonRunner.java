09b8ed7922205482f3d8da1dc118598a5b936b51
Usama2627 <www.usamaattar2627@gmail.com>
2023-06-27 20:11:28 +0530

    Task

---
+++ PersonRunner.java
@@ -0,0 +1,80 @@
+class PersonRunner {
+	
+	static String getName(){
+		String name="Usama";
+		System.out.println("The Name is: " + name);
+		return name;
+	}
+	
+	static int getAge(){
+		int age=24;
+		System.out.println("The Age is: " + age);
+		return age;
+	}
+	
+	static String getEmail(){
+		String email="www.usamaattar2627@gmail.com";
+		System.out.println("The Email is: " + email);
+		return email;
+	}
+	
+	static long getMobileNo(){
+		long mobile=7020404023L;
+		System.out.println("The Mobile Number is: " + mobile);
+		return mobile;
+	}
+	
+	static float getHeight() {
+		float height=5.6F;
+		System.out.println("The Height is: " + height);
+		return height;
+	}
+	
+	static float getWeight() {
+		float weight=64.5F;
+		System.out.println("The Weight is: " + weight);
+		return weight;
+	}
+	
+	static int getSalary() {
+		int salary=50000;
+		System.out.println("The Salary is: " + salary);
+		return salary;
+	}
+	
+	static String getBestFriendName() {
+		String friend="XYZ";
+		System.out.println("The Best Friend is: " + friend);
+		return friend;
+	}
+	
+	static String getFatherName() {
+		String father="ABCD";
+		System.out.println("The Father Name is: " + father);
+		return father;
+	}
+	
+	static String getMotherName() {
+		String mother="EFGH";
+		System.out.println("The Mother Name is: " + mother);
+		return mother;
+	}
+	
+	static String getAreaName() {
+		String area="Banglore";
+		System.out.println("The Area is: " + area);
+		return area;
+	}
+	
+	static String getEnemy() {
+		String enemy="AAA";
+		System.out.println("The Enemy is: " + enemy);
+		return enemy;
+	}
+	
+	static String getInspiration() {
+		String inspiration="ZZZ";
+		System.out.println("The Inspiration is: " + inspiration);
+		return inspiration;
+	}
+}
