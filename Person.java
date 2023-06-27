3c86834f8e1e8b94f60979da2c49172f519393d3
Usama2627 <www.usamaattar2627@gmail.com>
2023-06-27 20:07:26 +0530

    message

---
+++ Person.java
@@ -0,0 +1,22 @@
+class Person {
+	
+	public static void main(String[] Persons){
+		
+		System.out.println("Main function is running...");
+		PersonRunner.getName();
+		PersonRunner.getAge();
+		PersonRunner.getEmail();
+		PersonRunner.getMobileNo();
+		PersonRunner.getHeight();
+		PersonRunner.getWeight();
+		PersonRunner.getSalary();
+		PersonRunner.getBestFriendName();
+		PersonRunner.getFatherName();
+		PersonRunner.getMotherName();
+		PersonRunner.getAreaName();
+		PersonRunner.getEnemy();
+		PersonRunner.getInspiration();
+		
+	}
+	
+}
