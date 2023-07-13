9ba1bdae43a3c748e8458c6cf1aab3659f2da462
Usama2627 <www.usamaattar2627@gmail.com>
2023-07-13 23:19:59 +0530

    Delete Enum/src directory

--- Enum/src/com/xworkz/JobType.java
+++
@@ -1,6 +0,0 @@
-package com.xworkz;
-
-public enum JobType {
-	
-    FULL_TIME, PART_TIME, CONTRACT
-}
--- Enum/src/com/xworkz/TheaterType.java
+++
@@ -1,6 +0,0 @@
-package com.xworkz;
-
-public enum TheaterType {
-	
-	SINGLE_SCREEN, MULTIPLEX, 
-}
