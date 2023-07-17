665fd1e3996d9a6cfe5d392a79254c0cca36bee6
Usama2627 <www.usamaattar2627@gmail.com>
2023-07-18 01:04:43 +0530

    Update Xworkz.java

--- Inheritance/src/com/xworkz/isa/Xworkz.java
+++ Inheritance/src/com/xworkz/isa/Xworkz.java
@@ -3,16 +3,16 @@ package com.xworkz.isa;
 public class Xworkz extends Institute{
 
 	String founderName;
-	String BrancheName;
+	String branchName;
 	int noOfSTudentsInClass;
 
 
 	Xworkz(String name, String purpose, String teachingMode, String establishedIn,
-			String founderName, String BrancheName, int noOfSTudentsInClass) {
+			String founderName, String branchName, int noOfSTudentsInClass) {
 
 		super(name, purpose, teachingMode, establishedIn);
 		this.founderName = founderName;
-		this.BrancheName = BrancheName;
+		this.branchName = branchName;
 		this.noOfSTudentsInClass = noOfSTudentsInClass;
 	}
 
@@ -22,7 +22,7 @@ public class Xworkz extends Institute{
 		System.out.println("Institute Teaching mode : " + teachingMode);
 		System.out.println("Institute established in: " + establishedIn);
 		System.out.println("Institute Founder name: " + founderName);
-		System.out.println("Institute Branch name : " + BrancheName);
+		System.out.println("Institute Branch name : " + branchName);
 		System.out.println("Number of Students in Class : " + noOfSTudentsInClass);
 
 	}
