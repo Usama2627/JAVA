ee0fd85dae913be104683b29e97936fb798e792e
Usama2627 <www.usamaattar2627@gmail.com>
2023-11-18 01:19:13 +0530

    Practice

---
+++ Practice/src/com/eclipse/practice/LinkedLists.java
@@ -0,0 +1,37 @@
+package com.eclipse.practice;
+
+import java.util.LinkedList;
+
+public class LinkedLists {
+
+	public static void main(String[] args) {
+
+		LinkedList<String> list = new LinkedList<String>();
+
+		list.addFirst("a");
+		list.addFirst("is");
+		System.out.println(list);
+
+		list.addFirst("this");
+		list.addLast("LinkedList");
+		System.out.println(list);
+
+		System.out.println(list.size());
+
+		for (int i = 0; i < list.size(); i++) {
+			System.out.print(list.get(i) + " -> ");
+
+		}
+		System.out.println("null");
+
+		list.removeFirst();
+		System.out.println(list);
+
+		list.removeLast();
+		System.out.println(list);
+
+		list.remove(1);
+		System.out.println(list);
+	}
+
+}
